e1eb1150cb83f8184b6d74895aceaecb6a788a88
Cody Christ <cr999c999@gmail.com>
2024-06-01 10:08:44 -0700

    Completed PDF embedding / vectorDB integration

--- app/worker.ts
+++ app/worker.ts
@@ -55,6 +55,22 @@ Generate a concise answer for a given question based solely on the provided sear
 If there is nothing in the context relevant to the question at hand, just say "Hmm, I'm not sure." Don't try to make up an answer.`;
 
 const embedPDF = async (pdfBlob: Blob) => {
+  const pdfLoader = new WebPDFLoader(pdfBlob, { parsedItemSeparator: " " });
+  const docs = await pdfLoader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 50,
+  });
+
+  const splitDocs = await splitter.splitDocuments(docs);
+
+  self.postMessage({
+    type: "log",
+    data: splitDocs,
+  });
+
+  await vectorstore.addDocuments(splitDocs);
 };
 
 const _formatChatHistoryAsMessages = async (
@@ -81,6 +97,103 @@ const queryVectorStore = async (
     devModeTracer?: LangChainTracer;
   },
 ) => {
+  const text = messages[messages.length - 1].content;
+  const chatHistory = await _formatChatHistoryAsMessages(messages.slice(0, -1));
+
+  const responseChainPrompt =
+    modelProvider === "ollama"
+      ? ChatPromptTemplate.fromMessages<{
+          context: string;
+          chat_history: BaseMessage[];
+          question: string;
+        }>([
+          ["system", OLLAMA_RESPONSE_SYSTEM_TEMPLATE],
+          new MessagesPlaceholder("chat_history"),
+          ["user", `{input}`],
+        ])
+      : ChatPromptTemplate.fromMessages<{
+          context: string;
+          chat_history: BaseMessage[];
+          question: string;
+        }>([
+          ["system", WEBLLM_RESPONSE_SYSTEM_TEMPLATE],
+          [
+            "user",
+            "When responding to me, use the following documents as context:\n<context>\n{context}\n</context>",
+          ],
+          [
+            "ai",
+            "Understood! I will use the documents between the above <context> tags as context when answering your next questions.",
+          ],
+          new MessagesPlaceholder("chat_history"),
+          ["user", `{input}`],
+        ]);
+
+  const documentChain = await createStuffDocumentsChain({
+    llm: chatModel,
+    prompt: responseChainPrompt,
+    documentPrompt: PromptTemplate.fromTemplate(
+      `<doc>\n{page_content}\n</doc>`,
+    ),
+  });
+
+  const historyAwarePrompt =
+    modelProvider === "ollama"
+      ? ChatPromptTemplate.fromMessages([
+          new MessagesPlaceholder("chat_history"),
+          ["user", "{input}"],
+          [
+            "user",
+            "Given the above conversation, generate a natural language search query to look up in order to get information relevant to the conversation. Do not respond with anything except the query.",
+          ],
+        ])
+      : ChatPromptTemplate.fromMessages([
+          new MessagesPlaceholder("chat_history"),
+          [
+            "user",
+            "Given the above conversation, rephrase the following question into a standalone, natural language query with important keywords that a researcher could later pass into a search engine to get information relevant to the conversation. Do not respond with anything except the query.\n\n<question_to_rephrase>\n{input}\n</question_to_rephrase>",
+          ],
+        ]);
+
+  const historyAwareRetrieverChain = await createHistoryAwareRetriever({
+    llm: chatModel,
+    retriever: vectorstore.asRetriever(),
+    rephrasePrompt: historyAwarePrompt,
+  });
+
+  const retrievalChain = await createRetrievalChain({
+    combineDocsChain: documentChain,
+    retriever: historyAwareRetrieverChain,
+  });
+
+  const fullChain = RunnableSequence.from([
+    retrievalChain,
+    new RunnablePick("answer"),
+  ]);
+
+  const stream = await fullChain.stream(
+    {
+      input: text,
+      chat_history: chatHistory,
+    },
+    {
+      callbacks: devModeTracer !== undefined ? [devModeTracer] : [],
+    },
+  );
+
+  for await (const chunk of stream) {
+    if (chunk) {
+      self.postMessage({
+        type: "chunk",
+        data: chunk,
+      });
+    }
+  }
+
+  self.postMessage({
+    type: "complete",
+    data: "OK",
+  });
 };
 
 // Listen for messages from the main thread
